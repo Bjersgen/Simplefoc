0550c3dfc1b2b6ba9f196f7468a5b577256142ff
bjersgen <601059768@qq.com>
2022-02-28 18:20:21 +0800

    foc1.0

--- NUCLEO-H743ZI2_FOC/code/MDK-ARM/US_digital_1000.c
+++ NUCLEO-H743ZI2_FOC/code/MDK-ARM/US_digital_1000.c
@@ -18,8 +18,8 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 			//码盘读数与计时器读数  差分码盘
             
      
-            Encoder_count_Motor1 =  (int16_t)(__HAL_TIM_GET_COUNTER(&htim4));
-            __HAL_TIM_SET_COUNTER(&htim4,0);
+			Encoder_count_Motor1 =  (int16_t)(__HAL_TIM_GET_COUNTER(&htim4));
+			__HAL_TIM_SET_COUNTER(&htim4,0);
            
 
           
